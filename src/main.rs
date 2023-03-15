1fa5de9f659f3cbcf7c144dd2719fee92fe05085
Michael Peters <michaelpeterswa@icloud.com>
2023-03-15 15:24:30 -0700

    fix: starting ci

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+use std::env;
+use std::process::Command;
+use urlencoding::encode;
+
+fn main() {
+    // handle environment variables
+    let browser = match env::var("BROWSER") {
+        Ok(val) => val,
+        Err(_e) => "".to_string(),
+    };
+
+    if browser.len() == 0 {
+        println!("Please Set $BROWSER Environment Variable");
+        return;
+    }
+
+    // handle command line arguments
+    let args: Vec<String> = env::args().collect();
+    let query_string = &args[1..args.len()].join(" ");
+    let query_string_encoded = encode(query_string);
+    let github_search_url = format!("https://github.com/search?q={}", query_string_encoded);
+    let _ = Command::new("open")
+        .arg("-a")
+        .arg(browser)
+        .arg(github_search_url)
+        .output();
+}
